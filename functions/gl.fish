fa3220dcfa7acf7fdd55c0afd6ea48cbceeeac8e
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-22 09:55:29 +0800

    fixed unbalanced quotes

--- functions/gl.fish
+++ functions/gl.fish
@@ -1,3 +1,3 @@
 function gl -d "pretty git log"
-  command git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+  command git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 end
